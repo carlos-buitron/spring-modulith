e2735df605842b4f700105d17a6ada2a362e4679
carlos-buitron <Carlos.Butron-EXPERIS@promart.pe>
2024-12-09 01:06:32 -0500

    First commit

---
+++ src/main/java/pe/springmodulith/ms/SpringModulithApplication.java
@@ -0,0 +1,13 @@
+package pe.springmodulith.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModulithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringModulithApplication.class, args);
+	}
+
+}
